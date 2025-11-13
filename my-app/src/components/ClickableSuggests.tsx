7163fa7363d28b29f502ad2810999d9435d6fe18
OhmuraYuta <ohmura.y.24kdgn@gmail.com>
2025-11-13 09:25:31 +0000

    add: クリックできるコンポーネント

---
+++ my-app/src/components/ClickableSuggests.tsx
@@ -0,0 +1,34 @@
+'use client';
+
+import { Platform } from "./DeepDivePlatformSelector";
+import { useFetchSuggests } from "@/hooks/useFetchSuggests";
+import ChildSuggests from "./ChildSuggests";
+
+type Props = {
+  keyword: string;
+  platform: Platform;
+}
+
+export default function ClickableSuggests({ keyword, platform }: Props) {
+  const suggests = useFetchSuggests(keyword, platform);
+
+  return (
+    <div>
+      <button >btn</button>
+      <p>{platform}</p>
+      <ul>
+        {suggests.map((suggest, index) => (
+          <li key={index}>
+            <ChildSuggests
+              suggest={suggest}
+              platform={platform}
+            />
+          </li>
+        ))}
+      </ul>
+      <ul>
+
+      </ul>
+    </div>
+  )
+}
