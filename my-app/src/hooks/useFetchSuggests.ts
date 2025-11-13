f38ffea0723ed3a9082a39ebfd6ac5fe8df3c2db
OhmuraYuta <ohmura.y.24kdgn@gmail.com>
2025-11-13 13:21:55 +0000

    update: 同じものを削除

--- my-app/src/hooks/useFetchSuggests.ts
+++ my-app/src/hooks/useFetchSuggests.ts
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { Platform } from "@/components/DeepDivePlatformSelector";
 
 export function useFetchSuggests(keyword: string, platform: Platform) {
-  const [suggests, setSuggests] = useState([]);
+  const [suggests, setSuggests] = useState<string[]>([]);
   useEffect(() => {
     if (keyword === '') {
       setSuggests([]);
@@ -15,7 +15,10 @@ export function useFetchSuggests(keyword: string, platform: Platform) {
     const url = `/api/suggest?keyword=${encodeURIComponent(keyword)}&platform=${encodeURIComponent(platform)}`;
     fetch(url, {signal})
       .then((res) => res.json())
-      .then((data) => setSuggests(data))
+      .then((data: string[]) => {
+        data = data.filter(el => el !== keyword);
+        setSuggests(data);
+      })
       .catch((err) => {
         if (err.name !== 'AbortError') {
           console.error(err);
