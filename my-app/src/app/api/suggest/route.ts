433d257b1032ad1798c5f5cf8aa726c868f9b80f
OhmuraYuta <ohmura.y.24kdgn@gmail.com>
2025-11-13 09:22:16 +0000

    update: パラメーター追加

--- my-app/src/app/api/suggest/route.ts
+++ my-app/src/app/api/suggest/route.ts
@@ -1,19 +1,29 @@
 import { NextRequest, NextResponse } from "next/server";
-import { fetchGoogleSuggests, fetchAmazonSuggests, fetchRakutenSuggests, fetchYahooSuggests } from '@/lib/utils';
+import { fetchGoogleSuggests, fetchAmazonSuggests, fetchRakutenSuggests, fetchYahooSuggests, getFuncByPlatform } from '@/lib/utils';
 
 export async function GET(request: NextRequest) {
     const { searchParams } = new URL(request.url);
     const keyword = searchParams.get('keyword');
+    const platform = searchParams.get('platform');
 
-    const gRes = await fetchGoogleSuggests(keyword);
-    const aRes = await fetchAmazonSuggests(keyword);
-    const rRes = await fetchRakutenSuggests(keyword);
-    const yRes = await fetchYahooSuggests(keyword);
-    const res = {
-        google: gRes,
-        amazon: aRes,
-        rakuten: rRes,
-        yahoo: yRes,
+    console.log(platform)
+
+    if (platform === null) {
+        const gRes = await fetchGoogleSuggests(keyword);
+        const aRes = await fetchAmazonSuggests(keyword);
+        const rRes = await fetchRakutenSuggests(keyword);
+        const yRes = await fetchYahooSuggests(keyword);
+        const res = {
+            google: gRes,
+            amazon: aRes,
+            rakuten: rRes,
+            yahoo: yRes,
+        }
+        return NextResponse.json(res);
+    } else {
+        const fetchSuggests = getFuncByPlatform(platform);
+        const res = await fetchSuggests(keyword);
+        return NextResponse.json(res);
     }
-    return NextResponse.json(res);
+
 }
