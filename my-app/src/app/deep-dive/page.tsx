3811ef48399c5bce2b67588065a6add92bca2a06
OhmuraYuta <ohmura.y.24kdgn@gmail.com>
2025-11-13 09:24:25 +0000

    update: 型定義

--- my-app/src/app/deep-dive/page.tsx
+++ my-app/src/app/deep-dive/page.tsx
@@ -2,19 +2,14 @@
 import Link from "next/link";
 import { useState } from "react";
 import DeepDivePlatformSelector, {Platform} from "@/components/DeepDivePlatformSelector";
-import DeepDiveSuggests from "@/components/DeepDiveSuggests";
+import DeepDiveSuggests, { Suggests } from "@/components/DeepDiveSuggests";
 import DeepDiveKeywordInput from "@/components/DeepDiveKeywordInput";
-
-// type DeepDiveSuggests = {
-//     suggests: object[];
-//     set
-// };
 
 export default function DeepDivePage() {
     const [platform, setPlatform] = useState<Platform>('google');
-    const [suggests, setSuggests] = useState<object[]>([]);
+    const [suggests, setSuggests] = useState<Suggests[]>([]);
     return (
-        <div>
+        <div className="space-y-2">
             <h1>深堀り</h1>
             <Link href="/">トップへ</Link>
             <DeepDivePlatformSelector
