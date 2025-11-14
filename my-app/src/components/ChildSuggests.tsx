a7af551ed20c43bc73814f387a0cfdf3873d3ee6
OhmuraYuta <ohmura.y.24kdgn@gmail.com>
2025-11-14 07:55:52 +0000

    update: Setに含まれないsuggestのみ表示

--- my-app/src/components/ChildSuggests.tsx
+++ my-app/src/components/ChildSuggests.tsx
@@ -1,15 +1,18 @@
 'use client';
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Platform } from "./DeepDivePlatformSelector";
+import { SuggestsSet } from "@/components/ClickableSuggests"
 
 type Props = {
   suggest: string;
   platform: Platform;
+  allSuggests: SuggestsSet;
+  setAllSuggests: React.Dispatch<React.SetStateAction<SuggestsSet>>;
 }
 
-export default function ChildSuggests({ suggest, platform }: Props) {
+export default function ChildSuggests({ suggest, platform, allSuggests, setAllSuggests }: Props) {
 
-  const [suggests, setSuggests] = useState([]);
+  const [suggests, setSuggests] = useState<string[]>([]);
   const [isDisplayed, setIsDisplayed] = useState(false);
 
   useEffect(() => {
@@ -18,14 +21,31 @@ export default function ChildSuggests({ suggest, platform }: Props) {
   
   async function fetchSuggests(url: string) {
     const res = await fetch(url);
-    const data = await res.json();
-    setSuggests(data);
+    const data: string[] = await res.json();
+    const difference: string[] = data.filter(el => !allSuggests.has(el));
+    setSuggests(difference);
+
+    setAllSuggests(prevAllSuggests => {
+      return new Set([...prevAllSuggests, ...difference]);
+    });
   }
 
   const handleClick = () => {
     if (isDisplayed === false) {
       const url = `/api/suggest?keyword=${encodeURIComponent(suggest)}&platform=${encodeURIComponent(platform)}`;
       fetchSuggests(url);
+    } else {
+      setAllSuggests(prevAllSuggests => {
+        let newSet = new Set(prevAllSuggests);
+
+        for (const suggest of suggests) {
+          newSet.delete(suggest);
+        }
+
+        return newSet;
+      });
+
+      setSuggests([]);
     }
     setIsDisplayed(!isDisplayed);
   };
@@ -39,6 +59,8 @@ export default function ChildSuggests({ suggest, platform }: Props) {
               <ChildSuggests
                 suggest={suggest}
                 platform={platform}
+                allSuggests={allSuggests}
+                setAllSuggests={setAllSuggests}
               />
             </li>
           )) : ''
